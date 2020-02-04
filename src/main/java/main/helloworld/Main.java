2af72dd63323a9e105765a727441b121ce2599bc
Zeljko Trifunovic <zecatrifunovic@gmail.com>
2020-02-04 18:51:58 +0100

    Initial commit

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,18 @@
+package main.helloworld;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line argumentsrintln
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        System.out.println("Zeljko Trifunovic");
+    }
+    
+}
