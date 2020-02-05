db4407a66f843551290e91648e4f8a2d13b8b1f2
ZeljkoTrifunovic <zecatrifunovic@gmail.com>
2020-02-05 11:06:13 -0800

    domaci zadatak

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -13,6 +13,35 @@ public class Main {
         
         System.out.println("Hello World");
         System.out.println("Zeljko Trifunovic");
+
+System.out.println("Unesiti redni broj dana u nedelji (1-7):");
+        Scanner input = new Scanner(System.in);
+        int redniBrDana = input.nextInt();
+        switch(redniBrDana) {
+            case 1:
+                System.out.println("Ponedeljak");
+                break;
+            case 2:
+                System.out.println("Utorak");
+                break;
+            case 3:
+                System.out.println("Sreda");
+                break;
+            case 4:
+                System.out.println("Cetvrtak");
+                break;
+            case 5:
+                System.out.println("Petak");
+                break;
+            case 6:
+                System.out.println("Subota");
+                break;
+            case 7:
+                System.out.println("Nedelja");
+                break;
+            default:
+                System.out.println("Nedelja ima samo 7 dana");
+}
     }
     
 }
